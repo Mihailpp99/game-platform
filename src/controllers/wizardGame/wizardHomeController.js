1dbd1c8927240f715195f8f2ec277195301d4973
Mihailpp99 <mihailpp99@gmail.com>
2022-04-06 10:52:39 +0300

    Connect back4app to my app

--- src/controllers/wizardGame/wizardHomeController.js
+++ src/controllers/wizardGame/wizardHomeController.js
@@ -2,4 +2,18 @@ import { wizardHomeTemplate } from "../../views/wizardGame/wizardHomeTemplate.js
 
 export const wizardHomeController = (ctx) => {
   ctx.renderMainContent(wizardHomeTemplate());
+  saveNewPerson();
 };
+
+async function saveNewPerson() {
+  const person = new Parse.Object("Person");
+
+  person.set("name", "John Snow");
+  person.set("age", 27);
+  try {
+    let result = await person.save();
+    alert("New object created with objectId: " + result.id);
+  } catch (error) {
+    alert("Failed to create new object, with error code: " + error.message);
+  }
+}
