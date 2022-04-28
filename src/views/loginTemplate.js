e89bb969405f116b501299dde0dcff8976641979
Mihailpp99 <mihailpp99@gmail.com>
2022-04-29 00:33:06 +0300

    Started doing the style of the login page

--- src/views/loginTemplate.js
+++ src/views/loginTemplate.js
@@ -2,21 +2,48 @@ import { html } from "../../node_modules/lit-html/lit-html.js";
 import { submitButton } from "../components/elements/buttons.js";
 
 export const loginTemplate = (ctx, logIn) => html`
-  <div id="authContainer">
-    <h3 class="centerTitle">Login</h3>
-
-    <form @submit=${(e) => logIn(e, ctx)}>
-      <div id="formWrapper">
-        <div class="formElementWrapper">
-          <label for="username" class="label">Username:</label>
-          <input type="text" name="username" class="baiscInput" />
-        </div>
-        <div class="formElementWrapper">
-          <label for="password" class="label">Password:</label>
-          <input type="password" name="password" class="baiscInput" />
-        </div>
-        <div style="text-align:center;">${submitButton("Login")}</div>
+  <div class="containerAuth">
+    <div class="ilustration-bg"></div>
+    <div id="authContainer">
+      <div class="logoImg centerElement" style="margin-top:5%;"></div>
+      <div id="navigationControlsLogin">
+        <span class="loginText">Login</span>
+        <span class="loginText">Register</span>
       </div>
-    </form>
+
+      <form @submit=${(e) => logIn(e, ctx)}>
+        <div id="formWrapper">
+          <div class="formElementWrapper">
+            <label for="username" class="label">Username:</label>
+            <input type="text" name="username" class="baiscInput" />
+          </div>
+          <div class="formElementWrapper">
+            <label for="password" class="label">Password:</label>
+            <input type="password" name="password" class="baiscInput" />
+          </div>
+          <div style="text-align:center;">${submitButton("Login")}</div>
+        </div>
+      </form>
+    </div>
   </div>
 `;
+
+// export const loginTemplate = (ctx, logIn) => html`
+//   <div id="authContainer">
+//     <h3 class="centerTitle">Login</h3>
+
+//     <form @submit=${(e) => logIn(e, ctx)}>
+//       <div id="formWrapper">
+//         <div class="formElementWrapper">
+//           <label for="username" class="label">Username:</label>
+//           <input type="text" name="username" class="baiscInput" />
+//         </div>
+//         <div class="formElementWrapper">
+//           <label for="password" class="label">Password:</label>
+//           <input type="password" name="password" class="baiscInput" />
+//         </div>
+//         <div style="text-align:center;">${submitButton("Login")}</div>
+//       </div>
+//     </form>
+//   </div>
+// `;
